41be1e9a2959924c1645a6515b43f201456310e8
Esen Sagynov <kadishmal@gmail.com>
2011-08-20 08:56:25 +0900

    - Enabled Google Analytics tracking code.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -13,7 +13,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
+	<meta name="language" content="<?php echo Yii::app()->language; ?>" />
 
 	<!-- blueprint CSS framework -->
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
@@ -26,19 +26,17 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
-    <?php
-//    <script type="text/javascript">
-//      var _gaq = _gaq || [];
-//      _gaq.push(['_setAccount', 'UA-3787071-5']);
-//      _gaq.push(['_trackPageview']);
-//
-//      (function() {
-//        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-//        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-//        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-//      })();
-//    </script>
-    ?>
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', '<?php echo $_SERVER['ga_track_code']; ?>']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
 </head>
 
 <body>
