51a93799e27c64b6b195e39c61c24d04283eea26
Esen Sagynov <kadishmal@gmail.com>
2011-09-17 17:08:58 +0900

    - Fixed the email layout.

--- protected/views/mail/newIdeaComment.php
+++ protected/views/mail/newIdeaComment.php
@@ -5,10 +5,10 @@
 
     echo CHtml::tag('div', array('style'=>'background:#FFF;border-bottom:1px solid #CCC;border-left:1px solid #CCC;border-right:1px solid #CCC;font-size:12px;width:580px;padding:20px'),
             Yii::t('ContentModule.forum', 'Hi,') . '<br /><br />' .
-            Yii::t('ContentModule.forum', 'There is a new comment on the <b>forum_name</b> post.', array('forum_name'=>$model->post->title)) . '<br /><br />' .
-            Yii::t('ContentModule.forum', 'user_name wrote: "message"', array('user_name'=>$model->user->first_name, 'message'=>$model->content)) . '<br /><br /><br />' .
+            Yii::t('ContentModule.forum', 'There is a new comment on the <b>forum_name</b> post.', array('forum_name'=>$title)) . '<br /><br />' .
+            Yii::t('ContentModule.forum', 'user_name wrote: "message"', array('user_name'=>$comment['first_name'], 'message'=>$comment['content'])) . '<br /><br /><br />' .
             CHtml::tag('span', array('style'=>'background:#FFF9D7;border:1px solid #E2C822;padding:10px;margin:15px 0'),
-                    CHtml::link(Yii::t('ContentModule.forum', 'See the comment thread'), $returnUrl, array('style'=>'color:#3B5998;text-decoration:none'))
+                    CHtml::link(Yii::t('ContentModule.forum', 'See the comment thread'), $link, array('style'=>'color:#3B5998;text-decoration:none'))
             ),
             Yii::t('ContentModule.forum', 'Thanks') . '<br />' .
             Yii::t('ContentModule.forum', 'Tilchi.com Team')
