ee77a439c5823b3afc066a4a04b89ba1f7675a5a
Esen Sagynov <kadishmal@gmail.com>
2011-10-03 20:57:24 +0900

    - Added the Tilchi search and translation from home page.

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -2,6 +2,12 @@
 
 class SiteController extends Controller
 {
+	const STATUS_NEW_FAIL = 0;
+	const STATUS_NEW_FAIL_RETRY = 1;
+	const STATUS_NEW_SUCCESS = 2;
+	const STATUS_NEW_EXISTS = 23000;
+	const STATUS_NEW_UNKNOWN_DB_ERROR = 3;
+	const STATUS_NEW_PHRASE_FAIL = 4;
 	/**
 	 * @return array action filters
 	 */
@@ -19,8 +25,12 @@ class SiteController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('deny', // do not allow logged in users to register or login
-				'actions'=>array('login'),
+			array('allow',  // allow all users to perform view actions
+				'actions'=>array('search', 'index', 'view'),
+				'users'=>array('*'),
+			),
+            array('allow',  // allow all users to perform view actions
+				'actions'=>array('new', 'edit', 'insert', 'map'),
 				'users'=>array('@'),
 			),
             array('deny', // do not allow guest users to logout
@@ -54,8 +64,6 @@ class SiteController extends Controller
 	 */
 	public function actionIndex()
 	{
-		// renders the view file 'protected/views/site/index.php'
-		// using the default layout 'protected/views/layouts/main.php'
 		$this->render('index');
 	}
 
@@ -92,4 +100,321 @@ class SiteController extends Controller
 		}
 		$this->render('contact',array('model'=>$model));
 	}
+	public function actionSearch()
+    {
+        $results = array();
+		$results['count'] = 0;
+
+        if(isset($_POST['Tilchi']))
+		{
+            $searchPhrase = $_POST['Tilchi']['phrase'];
+
+            if (strlen(trim($searchPhrase)) > 0)
+            {
+				$fromLang = $_POST['Tilchi']['fromLang'];
+				//$toLang = $_POST['Tilchi']['toLang'];
+
+				$fromLang = Language::model()->find('id = :id', array(':id'=>$fromLang));
+				//$toLang = Language::model()->find('id = :id', array(':id'=>$toLang));
+
+				if ($fromLang)
+				{
+					$idx = 'idx_phrases';
+					// Russiang and Kyrgyz languages have their own customized indexes
+					if ($fromLang->abbreviation == 'ru' || $fromLang->abbreviation == 'ky')
+					{
+						$idx .= '_' . $fromLang->abbreviation;
+					}
+
+					$search = Yii::App()->search;
+
+					$search->select('id')->
+						from($idx)->
+						where($searchPhrase . '*')->
+						filters(array('language_id'=>$fromLang->id))->
+						limit(0, 10, 1000);
+
+					$searchResults = $search->search();
+					$results['count'] = $searchResults->getTotal();
+					$ids = implode(',', $searchResults->getIdList());
+
+					if ($results['count'] > 0)
+					{
+						$phrases = Phrase::model()->with('language')->findAll(array(
+							'condition'=>'t.id IN (' . $ids . ')',
+							'order'=>'FIELD(t.id,' . $ids . ')'
+						));
+
+						$results['phrases'] = array();
+
+						$lang = Yii::app()->language;
+
+						foreach($phrases as $phrase)
+						{
+							$results['phrases'][] = array(
+								'phrase'=>$phrase->phrase,
+								'language'=>$phrase->language->$lang,
+								'langAbbr'=>$phrase->language->abbreviation
+							);
+						}
+					}
+					else{
+//						$results['status'] = Yii::t('tilchi', 'Sorry, but the phrase <b>_phrase</b> has not been found. If you are sure this phrase exists, request its translation below, and our community members will make sure it is translated.', array(
+//							'_phrase'=>$searchPhrase
+//						));
+						$results['status'] = Yii::t('tilchi', 'The phrase <b>_phrase</b> has not been found, but we have already added it to our to-translate list.', array('_phrase'=>$searchPhrase));
+					}
+
+					if (!Yii::app()->user->isGuest)
+					{
+						$searchHistory = new PhraseSearchHistory;
+						$searchHistory->user_id = Yii::app()->user->id;
+						$searchHistory->phrase = $searchPhrase;
+						$searchHistory->language_id = $fromLang->id;
+						$searchHistory->search_date = time();
+
+						$searchHistory->save();
+					}
+				}
+            }
+            else{
+				$results['status'] = Yii::t('tilchi', 'No translation found');
+            }
+
+            if(isset($_POST['ajax']) && $_POST['ajax'] === 'tilchi-search-form')
+            {
+                echo CJSON::encode($results);
+            }
+            else{
+                $this->render('_viewSearchResults', array(
+                    'dataProvider'=>$dataProvider
+                ));
+            }
+        }
+    }
+	public function actionView()
+	{
+		if (isset($_GET['language']))
+		{
+			$searchPhrase = $_GET['phrase'];
+			$toLang = isset($_GET['toLang']) ? $_GET['toLang'] : false;
+
+			$lang = Language::model()->findByPk($_GET['language']);
+
+			if ($lang)
+			{
+				$idx = 'idx_phrases';
+				// Russiang and Kyrgyz languages have their own customized indexes
+				if ($lang->abbreviation == 'ru' || $lang->abbreviation == 'ky')
+				{
+					$idx .= '_' . $lang->abbreviation;
+				}
+
+				$search = Yii::App()->search;
+
+				$search->select('id')->
+					from($idx)->
+					where($searchPhrase)->
+					filters(array('language_id'=>$lang->id))->
+					limit(0, 1, 1000);
+
+				$searchResults = $search->search();
+
+				$sysLang = Yii::app()->language;
+				$results = array();
+				$results['count'] = $searchResults->getTotal();
+
+				if ($results['count'] > 0)
+				{
+					$results['translations'] = array();
+					$params = array(
+						':id'=>implode(',', $searchResults->getIdList())
+					);
+					$condition = 't.id = :id';
+
+					if ($toLang !== false)
+					{
+						$params[':language_id'] = $toLang;
+						$condition .= ' AND translations.translation_language_id = :language_id';
+					}
+
+					$phrase = Phrase::model()->with('translations', 'translations.translationPhrase', 'translations.translationLanguage')->find($condition, $params);
+
+					if ($phrase)
+					{
+						foreach ($phrase->translations as $translation)
+						{
+							$results['translations'][] = array(
+								'phrase'=>$translation->translationPhrase->phrase,
+								'language'=>$translation->translationLanguage->$sysLang
+							);
+						}
+					}
+
+					$results['translationsCount'] = count($results['translations']);
+
+					if ($results['translationsCount'] == 0)
+					{
+						$isPhrase = mb_strpos($searchPhrase, ' ');
+
+						$results['messages'] = array(
+							'noTranslation'=>Yii::t('tilchi', 'The word <b>_phrase</b> exists in our database, but no translation has been found for it. If you know the translation, help us improve Tilchi.com by adding your translation. You will be recorded as a contributor.', array('_phrase'=>$searchPhrase))
+						);
+					}
+				}
+
+				if (isset($_GET['ajax']) && $_GET['ajax'] == 'ajax')
+				{
+					echo CJSON::encode($results);
+				}
+				else{
+					$this->render('view', array(
+						'phrase'=>$searchPhrase,
+						'fromLang'=>$lang->$sysLang,
+						'fromLangId'=>$lang->id,
+						'sysLang'=>$sysLang,
+						'results'=>$results
+					));
+				}
+			}
+		}
+		else{
+			$this->actionIndex();
+		}
+	}
+	private function findId($phrase, $languageId)
+	{
+		$search = Yii::App()->search;
+
+		$search->select('id')->
+			from('idx_phrases')->
+			where($phrase)->
+			filters(array('language_id'=>$languageId))->
+			limit(0, 1, 1000);
+
+		return $search->search()->getIdList();
+	}
+	public function actionNew()
+    {
+        if(isset($_POST['Tilchi']))
+		{
+			$results = array();
+			$results['status'] = self::STATUS_NEW_FAIL;
+
+			$fromLang = $_POST['Tilchi']['fromLang'];
+			$toLang = $_POST['Tilchi']['toLang'];
+			$phrase = $_POST['Tilchi']['phrase'];
+			$translation = $_POST['Tilchi']['translation'];
+			$explanation = $_POST['Tilchi']['explanation'];
+
+			if (Language::model()->exists('id = :id', array(':id'=>$fromLang)) &&
+				Language::model()->exists('id = :id', array(':id'=>$toLang)))
+			{
+				$fromId = implode(',', $this->findId($phrase, $fromLang));
+
+				if ($fromId != '')
+				{
+					$toId = implode(',', $this->findId($translation, $toLang));
+					$user_id = Yii::app()->user->id;
+
+					// how many approved translations does the user have
+					$myTranslations = PhraseTranslation::model()->count(
+						'user_id = :user_id AND status = :status',
+						array(
+							':user_id'=>$user_id,
+							':status'=>PhraseTranslation::STATUS_NEW_APPROVED
+						)
+					);
+
+					if ($toId == '')
+					{
+						// the translation does not exists in the database,
+						// therefore insert it to DB
+						$translationPhrase = new Phrase('new');
+						$translationPhrase->phrase = $translation;
+						$translationPhrase->language_id = $toLang;
+						$translationPhrase->user_id = $user_id;
+						$translationPhrase->date = time();
+
+						$translationPhrase->status = $myTranslations > PhraseTranslation::MIN_TRANSLATOR_COUNT ? PhraseTranslation::STATUS_NEW_APPROVED : PhraseTranslation::STATUS_NEW_PENDING;
+
+						if ($translationPhrase->save())
+						{
+							$toId = $translationPhrase->id;
+						}
+						else{
+							$results['status'] = self::STATUS_NEW_PHRASE_FAIL;
+							$results['messages'] = array(
+								'title'=>Yii::t('tilchi', 'Translation Error'),
+								'message'=>Yii::t('tilchi', 'We could not save the translation you have submitted. Please try again.'),
+								'ok'=>Yii::t('tilchi', 'Ok')
+							);
+						}
+					}
+
+					// translation exists in DB, so just link them both direction
+					if ($toId != '')
+					{
+						$phraseTranslation = new PhraseTranslation;
+
+						$phraseTranslation->user_id = $user_id;
+						$phraseTranslation->phrase_id = $fromId;
+						$phraseTranslation->translation_phrase_id = $toId;
+						$phraseTranslation->translation_language_id = $toLang;
+						$phraseTranslation->date = time();
+						$phraseTranslation->explanation = $explanation;
+
+						// auto approve the translations if the user already has
+						// submitted the minimum number of translations.
+						$phraseTranslation->status = $myTranslations > PhraseTranslation::MIN_TRANSLATOR_COUNT ? PhraseTranslation::STATUS_NEW_APPROVED : PhraseTranslation::STATUS_NEW_PENDING;
+
+						try
+						{
+							if ($phraseTranslation->save())
+							{
+								$results['status'] = self::STATUS_NEW_SUCCESS;
+							}
+							else{
+								$results['status'] = self::STATUS_NEW_FAIL_RETRY;
+							}
+						}
+						catch(Exception $e)
+						{
+							if ($e->getCode() == self::STATUS_NEW_EXISTS)
+							{
+								$results['status'] = self::STATUS_NEW_EXISTS;
+								$results['messages'] = array(
+									'title'=>Yii::t('tilchi', 'Translation Error'),
+									'message'=>Yii::t('tilchi', 'Such translation already exists. Please enter another translation.'),
+									'ok'=>Yii::t('tilchi', 'Ok')
+								);
+							}
+							else{
+								$results['status'] = self::STATUS_NEW_UNKNOWN_DB_ERROR;
+							}
+						}
+
+						if ($results['status'] == self::STATUS_NEW_SUCCESS)
+						{
+							$pairPhraseTranslation = new PhraseTranslation;
+							$pairPhraseTranslation->user_id = $user_id;
+							$pairPhraseTranslation->phrase_id = $toId;
+							$pairPhraseTranslation->translation_phrase_id = $fromId;
+							$pairPhraseTranslation->translation_language_id = $fromLang;
+							$pairPhraseTranslation->date = $phraseTranslation->date;
+
+							try{
+								$pairPhraseTranslation->save();
+							}
+							catch(Exception $e){
+								// ignore the error.
+							}
+						}
+					}
+				}
+			}
+
+			echo CJSON::encode($results);
+		}
+    }
 }
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,3 +1,101 @@
-<?php $this->pageTitle=Yii::app()->name; ?>
+<?php
+    $this->pageTitle = Yii::app()->name;
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+	$cs = Yii::app()->clientScript;
+	$cs->registerCoreScript('cookie');
+	$cs->registerScript('tilchi-search',"
+		activateTilchiSearch('tilchi-search-form');
+	");
+?>
+
+<div id="tilchi">
+     <div class="tilchi-body">
+         <div class="frame">
+            <div class="title"><h2><?php
+				echo Yii::t('tilchi', 'Dictionary');
+
+				$lang = Yii::app()->language;
+
+				$langs = CHtml::listData(Language::model()->findAll(array(
+					'select'=>'id, ' . $lang
+				)), 'id', $lang);
+
+				echo CHtml::dropDownList('fromLang', (isset(Yii::app()->request->cookies['Tilchi_fromLang']) ? Yii::app()->request->cookies['Tilchi_fromLang']->value : 1), $langs) .
+					CHtml::tag('span', array('style'=>'margin:0 10px', 'class'=>'sprite switch'), '') . CHtml::dropDownList('toLang', (isset(Yii::app()->request->cookies['Tilchi_toLang']) ? Yii::app()->request->cookies['Tilchi_toLang']->value : 2), $langs);
+
+			?></h2></div><div id="tilchi-search">
+                <?php
+                    $form = $this->beginWidget('CActiveForm', array(
+                        'id'=>'tilchi-search-form',
+                        'action'=>'/site/search',
+						'focus'=>'#Tilchi_phrase'
+                    ));
+
+                    echo CHtml::hiddenField('Tilchi[fromLang]') .
+						CHtml::hiddenField('Tilchi[toLang]') .
+						CHtml::textField('Tilchi[phrase]', '', array('class'=>'textField')) .
+						CHtml::submitButton(Yii::t('site', 'Search'), array('class'=>'button big'));
+
+                    $this->endWidget();
+                ?><div id="results"></div>
+            </div><div class="body" id="search-container">
+				<div id="translation"></div>
+                <div id="add-translation">
+					<a class="block-button" id="add-text"><?php echo Yii::t('tilchi', 'Add translation'); ?></a>
+                    <?php
+					if (Yii::app()->user->isGuest)
+					{
+						echo CHtml::tag('p', array('id'=>'add-translation-form'), Yii::t('tilchi', 'In order to add translations you need to be <a href="_url">logged in</a>. If you are not our member yet, <a href="/user/register">join us</a> and start translating!', array('_url'=>Yii::app()->user->loginUrl[0])));
+					}
+					else{
+						$form = $this->beginWidget('CActiveForm', array(
+							'id'=>'add-translation-form',
+							'action'=>'/site/new'
+						));
+
+						echo CHtml::hiddenField('Tilchi[fromLang]') .
+							CHtml::hiddenField('Tilchi[toLang]') .
+							CHtml::hiddenField('Tilchi[phrase]') .
+							CHtml::label(Yii::t('tilchi', 'Enter the exact translation'), 'Tilchi_translation', array('class'=>'h3')) .
+							CHtml::textField('Tilchi[translation]', '', array('class'=>'textField')) .
+							CHtml::tag('div', array('id'=>'translationResults'), '') .
+							CHtml::tag('div', array('class'=>'hint'), Yii::t('tilchi', 'Please enter the precise translation and <b>only one at a time</b>. If there are more than one meanings, enter only one. Other meanings can be entered later. If you think there is no exact translation, leave this field empty and instead enter the explanation below.')) .
+							CHtml::label(Yii::t('tilchi', 'Enter the explanation'), 'Tilchi_explanation', array('class'=>'h3 top-10 block-button', 'id'=>'add-explanation')) .
+							'<div id="Tilchi_explanation_block">';
+						$this->widget('application.modules.content.extensions.tinymce.ETinyMce', array(
+							'name'=>'Tilchi[explanation]',
+							'id'=>'Tilchi_explanation',
+							'height'=>'100px',
+							'language'=>'ru',
+							'useSwitch'=>false,
+							//'contentCSS'=>'/tinyContent.css',
+							//'value'=>$model->content,
+							'plugins'=>array('spellchecker'),
+							'useSwitch'=>false,
+							'useCompression'=>true,
+							'options'=>array(
+								'theme'=>'advanced',
+								'skin'=>'o2k7',
+								'theme_advanced_toolbar_location'=>'bottom',
+								'theme_advanced_toolbar_align'=>'center',
+								'theme_advanced_buttons1'=>"bold,italic,underline,strikethrough,|,bullist,numlist,|,undo,redo,|,link,unlink,anchor,image,cleanup",
+								'theme_advanced_buttons2'=>'',
+								'theme_advanced_buttons3'=>''
+							)
+						));
+
+						echo CHtml::tag('div', array('class'=>'hint'), Yii::t('tilchi', 'If you think that the translation has to be clarified, please enter its explanation below. If there is no need for explanation, leave this field empty.')) .
+							'</div>' .
+							CHtml::tag('div', array('class'=>'top-10 text-right'),
+								CHtml::link(Yii::t('tilchi', 'Cancel'), '', array('class'=>'link-button')) .
+								CHtml::submitButton(Yii::t('tilchi', 'Add'), array('class'=>'button primary'))
+							);
+
+						$this->endWidget();
+					}
+                ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
