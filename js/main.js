74dc80fa2c16f5bf12f68f559d403461494b0887
Esen Sagynov <kadishmal@gmail.com>
2011-10-04 22:44:32 +0900

    - Testing other error messages for debugging.

--- js/main.js
+++ js/main.js
@@ -103,8 +103,8 @@ function doSearch(url, dataToSend, searchHandler, completeHandler)
 			searchHandler(data);
 		},
 		'error':function(jqXHR, textStatus, errorThrown){
-			alert(textStatus + ' ' + jqXHR.statusText + ' ' + jqXHR.responseText);
-			alert(jqXHR.getAllResponseHeaders());
+			alert("readyState: "+jqXHR.readyState+"\nstatus: "+jqXHR.status);
+			alert("responseText: "+jqXHR.responseText);
 		},
 		'complete':function()
 		{
