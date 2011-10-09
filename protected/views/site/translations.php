7dce9d8775a3b47344d7b564a6e849be45182af6
Esen Sagynov <kadishmal@gmail.com>
2011-10-09 14:43:19 +0900

    - Forgot to add the view files for translations.

---
+++ protected/views/site/translations.php
@@ -0,0 +1,18 @@
+<?php
+    $this->pageTitle = Yii::t('tilchi', 'Translations') . ' | ' . Yii::app()->name;
+?>
+
+<div id="tilchi">
+     <div class="tilchi-body">
+         <div class="frame">
+            <div class="title"><h2><?php echo Yii::t('tilchi', 'Translations');?></h2></div>
+			<div class="body">
+			<?php $this->widget('zii.widgets.CListView', array(
+				'id'=>'translations-list',
+				'dataProvider'=>$dataProvider,
+				'itemView'=>'_translations'
+			)); ?>
+			</div>
+        </div>
+    </div>
+</div>
