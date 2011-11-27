4647efd0e08273ce239ae10fe09729a3ea4e07b2
Esen Sagynov <kadishmal@gmail.com>
2011-11-27 12:55:14 +0900

    Added a template for a new user registration email template. Added translation strings as well.

--- protected/modules/user/messages/ru/register.php
+++ protected/modules/user/messages/ru/register.php
@@ -10,6 +10,12 @@ return array(
     'Password'=>'Пароль',
     'To have a strong <b>password</b>, it should consist of <b>6 or more</b> characters that you can remember. You can later use it to access your account at Tilchi.com web site.'=>'Чтобы создать <b>надежный пароль</b>, он должен состоять из <b>6 и более</b> символов, которые Вы сможете запомнить. В дальнейшем, используя этот пароль, Вы сможете заходить в свою учетную запись на сайте Tilchi.com.',
     'To protect your account, enter a <b>strong password</b>.'=>'Для защиты Вашей учетной записи введите <b>надежный пароль</b>.',
-	'Please choose your gender.'=>'Выберите свой пол.'
+	'Please choose your gender.'=>'Выберите свой пол.',
+    'You account details at Tilchi.com Online Dictionary site.'=>'Информация о Вашей учетной записи на сайте Онлайн Словаря Tilchi.com',
+    'Hi user_name,'=>'Здравствуйте, user_name!',
+    'Thank you for joining us on the adventure of learning languages.<br /><br />You can now start using Tilchi.com community site in its fullest potential. Start translating directly from the <a href="home_url">Tilchi.com home page</a>.<br /><br />If you have questions regarding how to use the site or have suggestions, feel free to use our <a href="forum_url">Community Forum</a>.'=>
+    'Благодарим Вас за то, что Вы присоединились в наше сообщество и решили улучшить свои знания кыргызского языка.<br /><br />Теперь Вы можете полноценно использовать сайт сообщества Tilchi.com. Начните переводить слова прямо с <a href="home_url">главного страницы Tilchi.com</a>.<br /><br />Если у Вас возникнут вопросы, касающиеся использования сайта, или будут какие-либо предложения по улучшению сайта Tilchi.com, пожалуйста, напишите нам на <a href="forum_url">форуме сообщества</a>.',
+    'Respectfully Yours,<br />Tilchi team.'=>'С уважением<br />Команда разработчиков Tilchi.',
+    'P.S. We have Facebook and Twitter accounts! Join us!'=>'Кстати, у нас есть Facebook и Twitter! Присоединяйтесь!'
 );
 ?>
