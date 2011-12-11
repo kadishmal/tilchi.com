b50f9fd77433fbc38f92249b9b731be90eb4513a
Esen Sagynov <kadishmal@gmail.com>
2011-12-11 12:01:47 +0900

    New message translations for reset password template.

--- protected/modules/user/messages/ru/login.php
+++ protected/modules/user/messages/ru/login.php
@@ -5,6 +5,38 @@ return array(
 	'Remember me'=>'Запомнить меня',
     'Make sure you have correctly entered your email address and password.'=>'Убедитесь, что Вы правильно ввели адрес Вашей электронной почты и пароль.',
     'Not registered yet?'=>'Еще не зарегистрированы?',
-	'Logout' => 'Выйти',
+    'Forgot password?'=>'Забыли пароль?',
+    'Restore Password'=>'Восстановить пароль',
+    'To restore your password enter your email address associated with your account. You will receive an email regarding how to reset your password.'=>'Чтобы восстановить пароль, введите электронный адрес, связанный с Вашей учетной записью. Вы получите электронное письмо с инструкцией о восстановлении своего пароля.',
+    'Restore'=>'Восстановить',
+    'No user has been found with this email address.'=>'Пользователь с таким электронным адресом не найден.',
+    'Email is required to restore the password.'=>'Для восстановления пароля необходимо ввести электронный адрес.',
+    'Restoring Password at Tilchi.com'=>'Восстановление пароля на сайте Tilchi.com',
+    'Restoring my password'=>'Восстановление моего пароля',
+    'Dear user_name,'=>'Здравствуйте, user_name!',
+    'This email was sent automatically by Tilchi.com in response to your request to recover your password. This is done for your protection; only you, the recipient of this email can take the next step in the password recover process.'=>
+    'Это письмо было автоматически отправлено сайтом Tilchi.com в ответ на Ваш запрос о восстановлении Вашего пароля. Такое предостережение сделано в целях Вашей защиты; только Вы, получатель этого письма, можете начать процесс восстановления пароля.',
+	'To reset your password and access your account either click on or copy and paste the following link into the address bar of your browser:'=>
+    'Чтобы восстановить свой пароль или получить доступ к Вашей учетной записи, нажмите на ссылку ниже или скопируйте ее и вставьте в адресную строку Вашего браузера:',
+    'If you did not forget your password, please ignore this email.'=>'Если Вы помните свой пароль, просто проигнорируйте это письмо.',
+    'The request was made from:'=>'Запрос был отправлен с:',
+    'Logout' => 'Выйти',
+    'Email sent!'=>'Письмо отправлено!',
+    'Go and check my inbox'=>'Проверить мою почту',
+    'Please check your inbox. We have sent an email regarding how to reset your password.<br /><br />You need to reset your password within 24 hours. Otherwise, you will have to request password reset again.'
+    =>'Проверьте свою электронную почту. Мы отправили Вам письмо с инструкцией о восстановлении Вашего пароля.<br /><br />Вам необходимо сменить пароль в течение 24 часов. Иначе, необходимо будет снова запрашивать смену пароля.',
+    'Sorry, we could not process your request. Please try again later.'=>'Мы не смогли обработать Ваш запрос. Пожалуйста, повторите запрос позже.',
+    'Confirm Email'=>'Подтвердите email',
+    'To continue with your password reset, you need to confirm your email address.'=>'Чтобы продолжить восстановление пароля, Вам необходимо подтвердить адрес своей электронной почты.',
+    'Continue'=>'Продолжить',
+    'Enter new password'=>'Введите новый пароль',
+    'Now enter your new password and confirm.'=>'Теперь введите новый пароль и подтвердите.',
+    'New'=>'Новый',
+    'Repeat'=>'Повторить',
+    'Confirm'=>'Подтвердить',
+    'Success!'=>'Успешно!',
+    'Congratulations! You have successfully reset your password. Now you can proceed to the login page.'=>'Поздравляем! Вы успешно восстановили свой пароль. Теперь Вы можете пройти на страницу входа на сайт.',
+    'Your password has already been reset with this URL. If you haven\'t reset it yourself, request another <a href="/user/signin/restorePassword">password reset</a> or cancel.'
+    =>'Ваш пароль был уже восстановлен. Если Вы не восстанавливали его, Вы можете еще раз запросить <a href="/user/signin/restorePassword">восстановление пароля</a>.'
 );
 ?>
