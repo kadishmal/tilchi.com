96b08b8a3b21b285c15150c3b8eceb95fc7bd840
Esen Sagynov <kadishmal@gmail.com>
2012-01-08 21:11:34 +0900

    Fixed a bug which did not display AJAX search results on home page.

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,20 +1,10 @@
 <?php
     $this->pageTitle = Yii::t('tilchi', 'Online Kyrgyz dictionary') . ' | ' . Yii::app()->name;
 
-    if (Yii::app()->user->getState('ajax_search'))
-    {
-        $script = "activateTilchiSearch('tilchi-search-form');";
-    }
-    else{
-        $script = "enableLanguageSwitch();";
-    }
-
     if (Yii::app()->user->getState('enable_shift_for_letters'))
     {
-        $script .= 'listenToLetters();';
+        Yii::app()->clientScript->registerScript('enable-shift-for-letters', 'listenToLetters();');
     }
-
-    Yii::app()->clientScript->registerScript('tilchi-search', $script);
 ?>
 
 <div id="tilchi">
@@ -54,65 +44,76 @@
                     $this->endWidget();
                 ?><div id="results"></div>
              </div><?php
-                 if (Yii::app()->user->getState('ajax_search') && Yii::app()->user->checkAccess('translator'))
+                 if (Yii::app()->user->getState('ajax_search'))
                  {
                      echo CHtml::tag('div', array('id'=>'search-container', 'class'=>'body'), false, false)
-                         . CHtml::tag('div', array('id'=>'translation'), '')
-                         . CHtml::tag('div', array('id'=>'add-translation'),
-                                CHtml::link(Yii::t('tilchi', 'Add translation'), '', array('id'=>'add-text', 'class'=>'block-button'))
-                                , false
-                         );
+                         . CHtml::tag('div', array('id'=>'translation'), '');
 
-                     $form = $this->beginWidget('CActiveForm', array(
-                         'id'=>'add-translation-form',
-                         'action'=>'/site/new'
-                     ));
+                     if (Yii::app()->user->checkAccess('translator'))
+                     {
+                         echo CHtml::tag('div', array('id'=>'add-translation'),
+                                 CHtml::link(Yii::t('tilchi', 'Add translation'), '', array('id'=>'add-text', 'class'=>'block-button'))
+                                 , false
+                             );
 
-                     echo CHtml::hiddenField('Tilchi[fromLang]') .
-                         CHtml::hiddenField('Tilchi[toLang]') .
-                         CHtml::hiddenField('Tilchi[phrase]') .
-                         CHtml::label(Yii::t('tilchi', 'Enter the exact translation'), 'Tilchi_translation', array('class'=>'h3')) .
-                         CHtml::textField('Tilchi[translation]', '', array('class'=>'textField', 'autocomplete'=>'off')) .
-                         CHtml::tag('div', array('id'=>'translationResults'), '') .
-                         CHtml::tag('div', array('class'=>'hint'), Yii::t('tilchi', 'Please enter the precise translation and <b>only one at a time</b>. If there are more than one meanings, enter only one. Other meanings can be entered later. If you think there is no exact translation, leave this field empty and instead enter the explanation below.')) .
-                         CHtml::label(Yii::t('tilchi', 'Enter the explanation'), 'Tilchi_explanation', array('class'=>'h3 top-10 block-button', 'id'=>'add-explanation')) .
-                         '<div id="Tilchi_explanation_block">';
-                     $this->widget('application.modules.content.extensions.tinymce.ETinyMce', array(
-                         'name'=>'Tilchi[explanation]',
-                         'id'=>'Tilchi_explanation',
-                         'height'=>'100px',
-                         'language'=>'ru',
-                         'useSwitch'=>false,
-                         //'contentCSS'=>'/tinyContent.css',
-                         //'value'=>$model->content,
-                         'plugins'=>array('spellchecker'),
-                         'useSwitch'=>false,
-                         'useCompression'=>true,
-                         'options'=>array(
-                             'theme'=>'advanced',
-                             'skin'=>'o2k7',
-                             'theme_advanced_toolbar_location'=>'bottom',
-                             'theme_advanced_toolbar_align'=>'center',
-                             'theme_advanced_buttons1'=>"bold,italic,underline,strikethrough,|,bullist,numlist,|,undo,redo,|,link,unlink,anchor,image,cleanup",
-                             'theme_advanced_buttons2'=>'',
-                             'theme_advanced_buttons3'=>''
-                         )
-                     ));
+                         $form = $this->beginWidget('CActiveForm', array(
+                             'id'=>'add-translation-form',
+                             'action'=>'/site/new'
+                         ));
 
-                     echo CHtml::tag('div', array('class'=>'hint'), Yii::t('tilchi', 'If you think that the translation has to be clarified, please enter its explanation below. If there is no need for explanation, leave this field empty.')) .
-                         '</div>' .
-                         CHtml::tag('div', array('class'=>'top-10 text-right'),
-                             CHtml::link(Yii::t('tilchi', 'Cancel'), '', array('class'=>'link-button')) .
-                                 CHtml::submitButton(Yii::t('tilchi', 'Add'), array('class'=>'button primary'))
-                         );
+                         echo CHtml::hiddenField('Tilchi[fromLang]') .
+                             CHtml::hiddenField('Tilchi[toLang]') .
+                             CHtml::hiddenField('Tilchi[phrase]') .
+                             CHtml::label(Yii::t('tilchi', 'Enter the exact translation'), 'Tilchi_translation', array('class'=>'h3')) .
+                             CHtml::textField('Tilchi[translation]', '', array('class'=>'textField', 'autocomplete'=>'off')) .
+                             CHtml::tag('div', array('id'=>'translationResults'), '') .
+                             CHtml::tag('div', array('class'=>'hint'), Yii::t('tilchi', 'Please enter the precise translation and <b>only one at a time</b>. If there are more than one meanings, enter only one. Other meanings can be entered later. If you think there is no exact translation, leave this field empty and instead enter the explanation below.')) .
+                             CHtml::label(Yii::t('tilchi', 'Enter the explanation'), 'Tilchi_explanation', array('class'=>'h3 top-10 block-button', 'id'=>'add-explanation')) .
+                             '<div id="Tilchi_explanation_block">';
+                         $this->widget('application.modules.content.extensions.tinymce.ETinyMce', array(
+                             'name'=>'Tilchi[explanation]',
+                             'id'=>'Tilchi_explanation',
+                             'height'=>'100px',
+                             'language'=>'ru',
+                             'useSwitch'=>false,
+                             //'contentCSS'=>'/tinyContent.css',
+                             //'value'=>$model->content,
+                             'plugins'=>array('spellchecker'),
+                             'useSwitch'=>false,
+                             'useCompression'=>true,
+                             'options'=>array(
+                                 'theme'=>'advanced',
+                                 'skin'=>'o2k7',
+                                 'theme_advanced_toolbar_location'=>'bottom',
+                                 'theme_advanced_toolbar_align'=>'center',
+                                 'theme_advanced_buttons1'=>"bold,italic,underline,strikethrough,|,bullist,numlist,|,undo,redo,|,link,unlink,anchor,image,cleanup",
+                                 'theme_advanced_buttons2'=>'',
+                                 'theme_advanced_buttons3'=>''
+                             )
+                         ));
 
-                     $this->endWidget();
+                         echo CHtml::tag('div', array('class'=>'hint'), Yii::t('tilchi', 'If you think that the translation has to be clarified, please enter its explanation below. If there is no need for explanation, leave this field empty.')) .
+                             '</div>' .
+                             CHtml::tag('div', array('class'=>'top-10 text-right'),
+                                 CHtml::link(Yii::t('tilchi', 'Cancel'), '', array('class'=>'link-button')) .
+                                     CHtml::submitButton(Yii::t('tilchi', 'Add'), array('class'=>'button primary'))
+                             );
 
-                     echo CHtml::closeTag('div')    // close <div id="add-translation">
-                          . CHtml::closeTag('div'); // close <div class="body">
+                         $this->endWidget();
 
-                     Yii::app()->clientScript->registerScript('allow-add-translation', "activateTranslation();");
+                         echo CHtml::closeTag('div');    // close <div id="add-translation">
+
+                         Yii::app()->clientScript->registerScript('allow-add-translation', "activateTranslation();");
+                     }
+
+                     // close <div class="body">
+                     echo CHtml::closeTag('div');
+
+                     Yii::app()->clientScript->registerScript('tilchi-search', "activateTilchiSearch('tilchi-search-form');");
                  }
+             else{
+                 Yii::app()->clientScript->registerScript('tilchi-search', "enableLanguageSwitch();");
+             }
              ?>
         </div>
     </div>
