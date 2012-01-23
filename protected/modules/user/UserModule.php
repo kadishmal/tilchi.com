e1c1411c0c7c08fe35aeb57abfd879f3ad4feda6
Esen Sagynov <kadishmal@gmail.com>
2012-01-23 13:23:59 +0900

    Added a function to calculate the relative date.

--- protected/modules/user/UserModule.php
+++ protected/modules/user/UserModule.php
@@ -35,4 +35,44 @@ class UserModule extends CWebModule
 		else
 			return false;
 	}
+    public static function getFormattedRelativeDate($date)
+    {
+        $diff = time() - $date;
+
+        if ($diff < 60){
+            return Yii::t('UserModule.user', 'one second ago|{n} seconds ago', $diff);
+        }
+
+        $diff /= 60;
+
+        if ($diff < 60){
+            return Yii::t('UserModule.user', 'one minute ago|{n} minutes ago', floor($diff));
+        }
+
+        $diff /= 60;
+
+        if ($diff < 24){
+            return Yii::t('UserModule.user', 'an hour ago|{n} hours ago', floor($diff));
+        }
+
+        $diff /= 24;
+
+        if ($diff < 7){
+            return Yii::t('UserModule.user', 'yesterday|{n} days ago', floor($diff));
+        }
+
+        if ($diff < 30){
+            return Yii::t('UserModule.user', 'last week|{n} weeks ago', floor($diff / 7));
+        }
+
+        $diff /= 30;
+
+        if ($diff < 12){
+            return Yii::t('UserModule.user', 'last month|{n} months ago', floor($diff));
+        }
+
+        $diff /= 12;
+
+        return Yii::t('UserModule.user', 'last year|{n} years ago', floor($diff));
+    }
 }
