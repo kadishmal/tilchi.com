ab213b3e6b92d8271c2cb50801071c6092d99406
Esen Sagynov <kadishmal@gmail.com>
2012-01-23 13:30:55 +0900

    User settings on the profile page are now loaded from the site settings table. User rights are also considered.

--- protected/modules/user/views/profile/index.php
+++ protected/modules/user/views/profile/index.php
@@ -2,7 +2,7 @@
 $this->pageTitle = Yii::t('UserModule.user', 'My profile') . ' | ' . Yii::app()->name;
 
 Yii::app()->clientScript->registerScript('enable-profile-edit', "
-    enableProfileEdit();
+    enableSettingsEdit();
 ");
 
 $this->menu=array(
@@ -25,28 +25,38 @@ $this->menu=array(
 		<div class="td"><?php echo '******' . CHtml::link(Yii::t('UserModule.user', 'Change'), '/user/edit/password', array('class'=>'link-button mLeft10')); ?></div>
 	</div>
     <hr />
-	<div class="row">
-		<div class="th"><?php echo Yii::t('UserModule.user', 'Notifications'); ?></div>
-		<div class="td">
-			<div class="settings checkbox"><input type="checkbox" id="subscr_post_comments" <?php echo $model->settings->subscr_post_comments ? 'checked' : ''; ?> /><label for="subscr_post_comments"><?php echo Yii::t('UserModule.user', 'Comments on my posts'); ?></label><span class="sprite msg" title="<?php echo Yii::t('UserModule.user', 'Receive email notifications, if someone comments on a post I have created or I have previously commented on.'); ?>"></span></div>
-		</div>
-	</div>
-    <div class="row">
-        <div class="th"><?php echo Yii::t('UserModule.user', 'Settings'); ?></div>
-        <div class="td">
-            <div class="settings checkbox"><input type="checkbox" id="ajax_search" <?php echo $model->settings->ajax_search ? 'checked' : ''; ?> /><label for="ajax_search"><?php echo Yii::t('UserModule.user', 'Activate Instant Translation'); ?></label><span class="sprite msg" title="<?php echo Yii::t('UserModule.user', 'Instant Translation allows to translate without refreshing or redirecting to another page.'); ?>"></span></div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="th"></div>
-        <div class="td">
-            <div class="settings checkbox"><input type="checkbox" id="save_search_history" <?php echo $model->settings->save_search_history ? 'checked' : ''; ?> /><label for="save_search_history"><?php echo Yii::t('UserModule.user', 'Save my translation history'); ?></label><span class="sprite msg" title="<?php echo Yii::t('UserModule.user', 'You can easily see the statistics of which words you have already searched for and learn from them.'); ?>"></span></div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="th"></div>
-        <div class="td">
-            <div class="settings checkbox"><input type="checkbox" id="enable_shift_for_letters" <?php echo $model->settings->enable_shift_for_letters ? 'checked' : ''; ?> /><label for="enable_shift_for_letters"><?php echo Yii::t('UserModule.user', 'Use SHIFT key to input Kyrgyz letters'); ?></label><span class="sprite msg" title="<?php echo Yii::t('UserModule.user', 'For ease of use, you can press SHIFT+о to input Kyrgyz letter \'ө\', or SHIFT+н or SHIFT+у to input Kyrgyz letters \'ң\' or \'ү\', respectively.'); ?>"></span></div>
-        </div>
-    </div>
+    <?php
+        $prevGroup = '';
+
+        foreach($userAccessibleSettings as $setting)
+        {
+            $group = substr($setting->module, strlen($this->module->id . '.' . $this->id . '.'));
+            $CSSClass = SiteSettings::getCSSClass($setting->data_type);
+            $htmlOptions = array('type'=>$CSSClass, 'id'=>$setting->name);
+
+            if ($setting->data_type == SiteSettings::TYPE_CHECKBOX)
+            {
+                if ($setting->default_value)
+                {
+                    $htmlOptions['checked'] = 'checked';
+                }
+            }
+            else{
+                $htmlOptions['value'] = $setting->default_value;
+            }
+
+            echo CHtml::tag('div', array('class'=>'row'),
+                CHtml::tag('div', array('class'=>'th'), ($group != $prevGroup ? Yii::t('UserModule.user', ucfirst($group)) : ''))
+                . CHtml::tag('div', array('class'=>'td'),
+                    CHtml::tag('div', array('class'=>'settings ' . $CSSClass),
+                        CHtml::tag('input', $htmlOptions, false)
+                        . CHtml::label(Yii::t('UserModule.user', $setting->en_label), $setting->name)
+                        . CHtml::tag('span', array('class'=>'sprite msg', 'title'=>Yii::t('UserModule.user', $setting->en_hint)), '')
+                    )
+                )
+            );
+
+            $prevGroup = $group;
+        }
+    ?>
 </div>
