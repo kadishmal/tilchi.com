8516894dc29bb53de90210b26fb89e4c3a2fbf9d
Esen Sagynov <kadishmal@gmail.com>
2012-01-23 13:32:01 +0900

    User settings are now loaded from Site Settings.

--- protected/modules/user/controllers/ProfileController.php
+++ protected/modules/user/controllers/ProfileController.php
@@ -2,8 +2,8 @@
 
 class ProfileController extends Controller
 {
-	const SETTINGS_SET_STATUS_SUCCESS = 1;
-	const SETTINGS_SET_STATUS_FAIL = -1;
+	const REQUEST_SUCCESS = 1;
+	const REQUEST_FAIL = -1;
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
 	 * using two-column layout. See 'protected/views/layouts/column2.php'.
@@ -27,9 +27,9 @@ class ProfileController extends Controller
 	public function accessRules()
 	{
 		return array(
-            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+            array('allow',
 				'actions'=>array('index', 'setSettings'),
-				'users'=>array('@'),
+				'roles'=>array('member'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
@@ -41,47 +41,81 @@ class ProfileController extends Controller
 	 */
 	public function actionIndex()
 	{
-		$model = User::model()->with('settings')->findByPk(Yii::app()->user->id);
+        // get a list of profile settings this user has permission to alter
+        $userSettingsList = SiteSettings::model()->findAll(array(
+            'condition'=>'module LIKE :module',
+            'params'=>array(':module'=>'user.profile%'),
+            'order'=>'module, name'
+        ));
 
-        if (!$model->settings)
+        $userAccessibleSettings = array();
+        // check if this user has access to each of these settings
+        foreach($userSettingsList as $setting)
         {
-            $userSettings = new UserSettings;
-            $userSettings->user_id = $model->id;
-
-            if (!$userSettings->save())
+            if(Yii::app()->user->checkAccess($setting->auth_item))
             {
-                throw new CHttpException(404, Yii::t('UserModule.profile',
-                    'Could not process your request. Please try again later. If this happens regularly, please <a href="/forum/new/issue">report this issue</a>.'));
+                $userAccessibleSettings[$setting->id] = $setting;
             }
-
-            $model->settings = $userSettings;
         }
 
-        $this->render('index', array('model'=>$model));
+        $model = User::model()->with('settings')->findByPk(Yii::app()->user->id);
+
+        // retrieve user's own preferences set perviously for some of these settings
+        foreach($model->settings as $setting)
+        {
+            $userAccessibleSettings[$setting->setting_id]->default_value = $setting->value;
+        }
+
+        $this->render('index', array(
+            'model'=>$model,
+            'userAccessibleSettings'=>$userAccessibleSettings
+        ));
 	}
 
 	public function actionSetSettings()
 	{
 		$results = array();
-        $results['status'] = self::SETTINGS_SET_STATUS_FAIL;
+        $results['status'] = self::REQUEST_FAIL;
 
 		if(isset($_POST['User']))
 		{
             $target = $_POST['User']['t'];
 			$value = $_POST['User']['v'];
+            $user_id = Yii::app()->user->id;
 
-            $model = UserSettings::model()->findByAttributes(array(
-                'user_id'=>Yii::app()->user->id
-            ));
+            $setting = SiteSettings::model()->find(
+                'module LIKE :module AND name = :name',
+                array(
+                    ':module'=>$this->module->id . '.' . $this->id . '%',
+                    ':name'=>$target
+                )
+            );
 
-            if ($model && $model->hasAttribute($target))
+            if ($setting && Yii::app()->user->checkAccess($setting->auth_item))
             {
-                $model->$target = ($value == 'true' ? true : false);
+                $userSetting = UserSettings::model()->findByAttributes(array(
+                    'user_id'=>$user_id,
+                    'setting_id'=>$setting->id
+                ));
 
-                if ($model->save())
+                if (!$userSetting)
                 {
-                    $results['status'] = self::SETTINGS_SET_STATUS_SUCCESS;
-                    Yii::app()->user->setState($target, $model->$target);
+                    $userSetting = new UserSettings;
+                    $userSetting->user_id = $user_id;
+                    $userSetting->setting_id = $setting->id;
+                }
+
+                if ($setting->data_type == SiteSettings::TYPE_CHECKBOX)
+                {
+                    $value = ($value == 'true' ? 1 : 0);
+                }
+
+                $userSetting->value = $value;
+
+                if ($userSetting->save())
+                {
+                    $results['status'] = self::REQUEST_SUCCESS;
+                    Yii::app()->user->setState($setting->name, $value);
                 }
             }
 		}
