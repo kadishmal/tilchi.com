28eafb75fea103bb42ccd228de883b07bce8b8e6
Esen Sagynov <kadishmal@gmail.com>
2012-01-29 22:52:15 +0900

    Displayed user gravatar on forum comments.

--- protected/modules/content/views/forum/_postComments.php
+++ protected/modules/content/views/forum/_postComments.php
@@ -4,10 +4,10 @@
     {
         echo '<div class="comment' . ($comment->status == Comment::STATUS_PENDING ? ' pending' : '') . '" id="comment-' . $comment->id . '">' .
             '<div class="author">' .
-                '<img class="avatar" src="' . $this->module->cssAssetUrl . '/' . ($comment->user->avatar > 0 ? $comment->user->avatar : '') . 'user.png" />' .
+                '<img class="avatar" src="' . $comment->user->getGravatar(80) . '" />' .
                 '<div class="name">';
                 if ($comment->user_id){
-                    echo $comment->user->first_name . ' ' . mb_substr($comment->user->last_name, 0, 1, 'UTF-8') . '.';
+                    echo $comment->user->getName();
                 }
                 else{
                     echo $comment->author;
