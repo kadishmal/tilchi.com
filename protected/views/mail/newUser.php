7a773c4c551c08cd86197c4c10f66b95bf547165
Esen Sagynov <kadishmal@gmail.com>
2012-02-01 22:21:19 +0900

    Fixed a bug in the email template which called request->hostInfo. In Yii console there is no request object. So hardcoded the host URL.

--- protected/views/mail/newUser.php
+++ protected/views/mail/newUser.php
@@ -1,5 +1,5 @@
 <?php
-    $home_url = Yii::app()->request->hostInfo;
+    $home_url = 'http://tilchi.com';
     echo CHtml::tag('div', array('style'=>'background:#F4F4F4;padding:8px;margin-bottom:15px;border-radius:8px;-moz-border-radius:8px;-webkit-border-radius:8px;'),
             CHtml::tag('div', array('style'=>'margin:8px 0 10px 10px;font-size:24px;color:#555'), 'Tilchi.com') .
             CHtml::tag('div', array('style'=>'border-radius:6px;-moz-border-radius:6px;-webkit-border-radius: 6px;border:1px solid #E9E9E9;background:#FFF;padding:15px 15px 20px 20px;'),
